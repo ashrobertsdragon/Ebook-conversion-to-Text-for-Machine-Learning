320fa5b9881c7980444195a258f4165185163dbe
ashrobertsdragon <128648365+ashrobertsdragon@users.noreply.github.com>
2023-12-01 05:24:27 +0000

    Added chapter number checking

--- main.py
+++ main.py
@@ -10,13 +10,133 @@ from PyPDF2 import PdfReader
 
 from common_functions import read_text_file, write_json_file, write_to_file
 
-CHAPTER_PATTERN = re.compile(r"^\s*chapter\s|\schapter\s*$", re.IGNORECASE)
+NOT_CHAPTER = ["about", "acknowledgements", "afterward", "annotation", "appendix", "assessment", "backmatter", "bibliography", "colophon", "conclusion", "contents", "contributors", "copyright", "cover", "credits", "dedication", "division", "endnotes", "epigraph", "errata", "footnotes", "forward", "frontmatter", "glossary", "imprintur", "imprint", "index", "introduction", "landmarks", "list", "notice", "page", "preamble", "preface", "prologue", "question", "rear", "revision", "table", "toc", "volume", "warning"]
 
-def detect_chapter(line: str) -> bool:
+def roman_to_int(roman: str) -> str:
   """
-  Detects if a line is a chapter header.
+  Convert a Roman numeral to an integer.
+
+  Arguments:
+    roman (str): A string representing the Roman numeral.
+  
+  Returns int: The integer value of the Roman numeral.
   """
-  return CHAPTER_PATTERN.match(line) is not None
+
+  roman_numerals = {
+    'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+  }
+
+  roman = roman.upper()
+
+  total = 0
+  prev_value = 0
+
+  for char in reversed(roman):
+    value = roman_numerals[char]
+
+    if value < prev_value:
+      total -= value
+    else:
+      total += value
+
+    prev_value = value
+
+
+  return str(total)
+
+def word_to_num_str(number_str: str) -> str:
+  """
+  Convert a spelled-out number without spaces or hyphens to a string of the integer.  The function supports numbers from 0 to 99 and is case insensitive.
+
+  Argumentss:
+    number_str (str): A string representing the spelled-out number.
+
+  Returns str: The string representation of the integer value of the spelled-out number.
+  """
+
+  num_words = {
+    'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+    'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9,
+    'ten': 10, 'eleven': 11, 'twelve': 12, 'thirteen': 13,
+    'fourteen': 14, 'fifteen': 15, 'sixteen': 16,
+    'seventeen': 17, 'eighteen': 18, 'nineteen': 19
+  }
+
+  tens_words = {
+    'twenty': 20, 'thirty': 30, 'forty': 40, 'fifty': 50,
+    'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90
+  }
+
+  number_str = number_str.lower()
+
+  total = 0
+  temp_word = ''
+
+  for char in number_str:
+    temp_word += char
+
+    if temp_word in num_words:
+      total += num_words[temp_word]
+      temp_word = ''
+    elif temp_word in tens_words:
+      total += tens_words[temp_word]
+      temp_word = ''
+
+  if temp_word:
+      raise ValueError(f"Unknown number word: {temp_word}")
+
+
+  return str(total)
+
+def is_chapter(s: str) -> bool:
+  """
+  Check if a string contains the word 'chapter', a Roman numeral, a spelled-out number, or a digit.
+
+  Args:
+    s (str): The string to check.
+
+  Returns bool: True if the string meets the criteria, False otherwise.
+  """
+
+  def is_number(s: str) -> bool:
+    """
+    Check if a string contains a number value either as digits, roman numerals, or spelled-out numbers.
+    """
+
+    
+    return s.isdigit() or is_roman_numeral(s) or is_spelled_out_number(s):
+  
+  def is_roman_numeral(word: str) -> bool:
+ 
+    try:
+        roman_to_int(word)
+
+      
+        return True
+    
+    except ValueError:
+    
+      
+      return False
+
+  def is_spelled_out_number(word: str) -> bool:
+    """Try to convert the word to an integer. If it's not a valid spelled-out number, it will raise a ValueError"""
+    
+    try:
+      word_to_num_str(word)
+      
+      
+      return True
+
+    except ValueError:
+    
+      
+      return False
+
+  words = s.lower().split()
+
+  
+  return any(word == "chapter" or is_number(word) for word in words)
 
 def convert_chapter_break(book_content: str) -> str:
   """
@@ -29,7 +149,7 @@ def convert_chapter_break(book_content: str) -> str:
 
   book_lines = book_content.split("\n")
   for i, line in enumerate(book_lines):
-    if detect_chapter(line) and i != 0:
+    if is_chapter(line) and i != 0:
       book_lines[i] = "***"
 
 
@@ -54,12 +174,10 @@ def read_epub(file_path: str) -> Tuple[str, dict]:
   metadata['title'] = next((item[0] for item in book.get_metadata('DC', 'title')), None)
   metadata['author'] = next((item[0] for item in book.get_metadata('DC', 'creator')), None)
 
-  blacklist = ["about", "acknowledgements", "afterward", "annotation", "appendix", "assessment", "backmatter", "bibliography", "colophon", "conclusion", "contents", "contributors", "copyright", "cover", "credits", "dedication", "division", "endnotes", "epigraph", "errata", "footnotes", "forward", "frontmatter", "glossary", "imprintur", "imprint", "index", "introduction", "landmarks", "list", "notice", "page", "preamble", "preface", "prologue", "question", "rear", "revision", "table", "toc", "volume", "warning"]
-
   for item in book.get_items():
     if item.get_type() == ebooklib.ITEM_DOCUMENT:
       item_href = item.file_name
-      if not any(blacklist_word in item_href.lower() for blacklist_word in blacklist):
+      if not any(not_chapter_word in item_href.lower() for not_chapter_word in NOT_CHAPTER):
         chapter_files.append(item_href)  
 
   for chapter in chapter_files:
