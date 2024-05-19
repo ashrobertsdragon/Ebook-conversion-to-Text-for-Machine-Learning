6c8c3191b4b6dc049c8930cd0caa7cc191ad2165
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-19 16:22:29 -0600

    convereted ebook conversion module to class

--- ebook_conversion/epub_conversion.py
+++ ebook_conversion/epub_conversion.py
@@ -1,50 +1,68 @@
 import ebooklib
 from ebooklib import epub
+from ebooklib.epub import EpubBook
 from bs4 import BeautifulSoup
 
+from .abtract_book import BookConversion
 from .chapter_check import is_chapter, is_not_chapter, NOT_CHAPTER
 from .ocr import encode_image, run_ocr
-from .text_conversion import desmarten_text
 
 
-def extract_epub_chapter_text(item, book) -> str:
-  """
-  Extracts text from a chapter item.
-  Arguments:
-    item: ebooklib item representing a chapter.
-  Returns string containing the text of the chapter.
-  """
+class EpubConversion(BookConversion):
+    
+    def _read_file(self, file_path: str) -> EpubBook:
+        return epub.read_epub(file_path, options={"ignore_ncx": True})
 
-  soup = BeautifulSoup(item.content, "html.parser")
-  elements = soup.find_all(["p", "img", "h1", "h2", "h3", "h4", "h5", "h6"])
-  for i, element in enumerate(elements[:3]):
-    if element.name == "img":
-      image_data=book.read_item(element["src"])
-      base64_images = [encode_image(image_data)]
-      text = run_ocr(base64_images)
-    else:
-      text = element.get_text().strip().lower()
-    if any(non_chapter_word in text for non_chapter_word in NOT_CHAPTER):
-      return ""
-    elif is_chapter(text):
-      starting_line = i + 1
-      return "\n".join(tag.get_text().strip() for tag in elements[starting_line:] if tag != "img")
-  return ""
+    def extract_text(self, element) -> str:
+        if element.name == "img":
+            base64_images = self._extract_images(element)
+            text = run_ocr(base64_images)
+        else:
+          text = element.get_text().strip().lower()
+        return text
+
+    def _extract_images(self, element) -> list:
+        image_data = self.book.read_item(element["src"])
+        return [encode_image(image_data)]
+    
+    def _process_chapter_text(self, item) -> str:
+        """
+        Extracts text from a chapter item.
+        
+            Args:
+                item: ebooklib item representing a chapter.
+            Returns string containing the text of the chapter.
+        """
+        TEXT_ELEMENTS = ["p", "img", "h1", "h2", "h3", "h4", "h5", "h6"]
+        soup = BeautifulSoup(item.content, "html.parser")
+        elements = soup.find_all(TEXT_ELEMENTS)
+      
+        for i, element in enumerate(elements[:self.MAX_LINES_TO_CHECK]):
+            text = self.extract_text(element)
+            if any(word for word in text if word in NOT_CHAPTER):
+                return ""
+            elif is_chapter(text):
+                starting_line = i + 1
+                return ("\n".join(
+                    tag.get_text().strip()
+                    for tag in elements[starting_line:]
+                    if tag != "img"
+                ))
+        return ""
+        
+    def split_chapters(self) -> str:
+        chapter: list = []
+        for item in self.book.get_items():
+            if (
+                item.get_type() == ebooklib.ITEM_DOCUMENT
+                and not is_not_chapter(item.file_name.lower(), self.metadata)
+            ):
+                chapter_text = self._process_chapter_text(item)
+                if chapter_text:
+                    chapter.append(self.clean_text(chapter_text))
+        return "\n***\n".join(chapter)
+
 
 def read_epub(file_path: str, metadata: dict) -> str:
-  """
-  Reads the contents of an epub file and returns it as a string.
-  Arguments:
-    file_path: Path to the epub file.
-  Returns the contents of the epub file as a string.
-  """
-
-  chapter_contents = []
-  book = epub.read_epub(file_path, options={"ignore_ncx": True})
-  for item in book.get_items():
-    if item.get_type() == ebooklib.ITEM_DOCUMENT and not is_not_chapter(item.file_name.lower(), metadata):
-      chapter_text = extract_epub_chapter_text(item, book)
-      if chapter_text:
-        chapter_text = desmarten_text(chapter_text)
-        chapter_contents.append(chapter_text)
-  return "\n***\n".join(chapter_contents)
+    epub_converter = EpubConversion(file_path, metadata)
+    return epub_converter.split_chapters()
