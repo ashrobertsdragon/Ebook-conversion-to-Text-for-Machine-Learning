b58a050f0fb14f413ff6dfbaa1cf923c15a47417
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-23 04:05:39 -0600

    updated setup.py for automatic versioning

--- setup.py
+++ setup.py
@@ -1,36 +1,28 @@
-from setuptools import setup, find_packages
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_version():
+    version_file = os.path.join("ebook2text", "VERSION.py")
+    with open(version_file) as f:
+        version_content = f.read()
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_content, re.M
+    )
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
 
 setup(
-    name='Ebook conversion to Text for Machine Learning',
-    version='1.0.0',
+    name="ebook2text",
+    version=get_version(),
+    description="Convert common book file types to text for machine learning",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ashlynn Antrobus",
+    author_email="ashlynn@prosepal.io",
     packages=find_packages(),
-    description='Converts common ebook file types to text for machine learning',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Ashlynn Antrobus',
-    author_email='ashlynn@prosepal.io',
-    url='https://github.com/ashrobertsdragon/Ebook-conversion-to-Text-for-Machine-Learning',
-    install_requires=[
-        "beautifulsoup4>=4.12.3",
-        "certifi>=2023.11.17",
-        "charset-normalizer>=2,<4",
-        "EbookLib==0.18",
-        "idna>2.5.0,<4",
-        "lxml>3.1.0",
-        "python-docx>=1.1.0",
-        "python-dotenv>=1.0.1",
-        "pillow>=10.2.0",
-        "requests==2.31.0",
-        "six==1.16.0",
-        "soupsieve>1.2",
-        "style==1.1.0",
-        "typing-extensions<3.10",
-        "update==0.0.1",
-        "urllib3>=1.21.1,<3"
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
 )
