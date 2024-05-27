a892407fd3c7866a4f022776f458888f4b9ffcd4
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-26 23:36:40 -0600

    _process_text now skips all paragraphs from 'chapters' flagged by is_not_chapter

--- ebook2text/docx_conversion.py
+++ ebook2text/docx_conversion.py
@@ -34,7 +34,8 @@ class DocxConverter(BookConversion):
 
     def __init__(self, file_path: str, metadata: dict):
         super().__init__(file_path, metadata)
-        self.paragraphs = self.extract_paragraphs()
+        self.paragraphs: list = self.extract_paragraphs()
+        self.non_chapter: bool = False
 
     def _read_file(self, file_path: str) -> Document:
         return docx.Document(file_path)
@@ -53,6 +54,7 @@ class DocxConverter(BookConversion):
         for run in paragraph.runs:
             if (
                 "<w:lastRenderedPageBreak/>" in run.element.xml
+                or "<w:pageBreakBefore/>" in run.element.xml
                 or "<w:br " in run.element.xml
             ):
                 return True
@@ -71,14 +73,15 @@ class DocxConverter(BookConversion):
             list: A list of base64-encoded strings, each representing an image
                 extracted from the paragraph.
         """
+        SCHEMA_STUB = "{http://schemas.openxmlformats.org/drawingml/2006/main}"
+        SCHEMA = f"'{SCHEMA_STUB}embed'"
         base64_images: list = []
-        SCHEMA = "{http://schemas.openxmlformats.org/drawingml/2006/main}"
         for run in paragraph.runs:
             for inline_shape in run.element.findall(
                 ".//a:blip", namespaces=docx.oxml.ns.nsmap
             ):
-                image_blip = inline_shape.get(f'"{SCHEMA}embed"')
-                image_part = self.doc.part.related_parts[image_blip]
+                image_blip = inline_shape.get(SCHEMA)
+                image_part = self.book.part.related_parts[image_blip]
                 stream = BytesIO(image_part.blob)
                 read_strm = stream.read()
                 base64_encoding = base64.b64encode(read_strm).decode("utf-8")
@@ -92,17 +95,61 @@ class DocxConverter(BookConversion):
         performs optical character recognition (OCR) on any images present in
         the paragraph, and returns the recognized text
         """
-        base64_images = self._extract_images(paragraph)
-        ocr_text = run_ocr(base64_images)
-        return ocr_text if ocr_text else paragraph.text.strip()
+        ocr_text: str = ""
+        base64_images: list = self._extract_images(paragraph)
+        if base64_images:
+            ocr_text = run_ocr(base64_images)
+        paragraph_text: str = paragraph.text.strip()
+        return ocr_text if ocr_text else paragraph_text
+
+    def _check_index(self, index: int) -> bool:
+        """
+        Checks if the given index exceeds a predefined maximum limit for lines
+        to check.
+
+        Args:
+            index (int): The index to be checked against the maximum limit.
+
+        Returns:
+            bool: True if the index is greater than or equal to the maximum
+                limit, False otherwise.
+        """
+        return index >= self.MAX_LINES_TO_CHECK
 
     def _is_start_of_chapter(self, text: str, index: int) -> bool:
-        if index >= self.MAX_LINES_TO_CHECK:
+        """
+        Checks if the given text marks the start of a new chapter based on
+        specific criteria.
+
+        Args:
+            text (str): The text to be checked for chapter header indicators.
+            index (int): The number of paragraphs since the last page break.
+
+        Returns:
+            bool: True if the text is considered the start of a chapter, False
+                otherwise.
+        """
+        if self._check_index(index):
             return False
-        return is_chapter(text) or not is_not_chapter(text, self.metadata)
+        return is_chapter(text)
+
+    def _is_non_chapter(self, text: str, index: int) -> bool:
+        """
+        Checks if the given text is not a chapter based on specific criteria.
+
+        Args:
+            text (str): The text to be checked for being front or back matter.
+            index (int): The paragraph index of the text within the chapter.
+
+        Returns:
+            bool: True if the text is not a chapter, False otherwise.
+        """
+        if self._check_index(index):
+            return False
+        return is_not_chapter(text, self.metadata)
 
     def _process_text(
-        self, paragraph_text: str, paragraph_chapter_index: int
+        self, paragraph_text: str, current_chapter_index: int
     ) -> Tuple[str, int]:
         """
         Process a paragraph's text to determine if it starts a new chapter and
@@ -110,21 +157,29 @@ class DocxConverter(BookConversion):
 
         Args:
             paragraph_text (str): The text of the paragraph to be processed.
-            paragraph_chapter_index (int): The index of the paragraph
+            current_chapter_index (int): The index of the paragraph
                 within the current chapter.
 
         Returns:
             Tuple[str, int]: A tuple containing the processed text and the
                 updated index of the paragraph within its chapter.
         """
-        if self._is_start_of_chapter(paragraph_text, paragraph_chapter_index):
-            paragraph_chapter_index = 0
-            processed_text = (
-                "***" if self.pages else self.clean_text(paragraph_text)
-            )
+        CHAPTER_SEPARATOR = "***"
+        current_chapter_index += 1
+
+        if self._is_non_chapter(paragraph_text, current_chapter_index):
+            processed_text = ""
+            self.non_chapter = True
+        elif self._is_start_of_chapter(paragraph_text, current_chapter_index):
+            current_chapter_index = 0
+            processed_text = CHAPTER_SEPARATOR if self.pages else ""
+            self.non_chapter = False
+        elif self.non_chapter:
+            processed_text = ""
         else:
-            processed_text = paragraph_text
-        return processed_text, paragraph_chapter_index
+            processed_text = self.clean_text(paragraph_text)
+
+        return processed_text, current_chapter_index
 
     def split_chapters(self) -> str:
         """Splits the paragraph list into chapters."""
@@ -132,7 +187,9 @@ class DocxConverter(BookConversion):
         current_page: list = []
         paragraph_chapter_index: int = 0
 
-        for paragraph in self.paragraphs:
+        for i, paragraph in enumerate(self.paragraphs):
+            if i > 20:
+                break
             paragraph_text = self.extract_text(paragraph)
             paragraph_chapter_index += 1
 
