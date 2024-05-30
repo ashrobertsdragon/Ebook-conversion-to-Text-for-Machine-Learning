0d8bd0e7129a2ea00fdef57a6b8a71945d034260
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-30 02:33:01 -0600

    FIX changed import to correct Document for correct type hint in _types

--- ebook2text/_types.py
+++ ebook2text/_types.py
@@ -1,5 +1,5 @@
 from bs4.element import ResultSet, Tag
-from docx import Document
+from docx.document import Document
 from docx.text.paragraph import Paragraph
 from ebooklib.epub import EpubBook, EpubItem
 from pdfminer.layout import LTChar, LTContainer, LTPage, LTText
