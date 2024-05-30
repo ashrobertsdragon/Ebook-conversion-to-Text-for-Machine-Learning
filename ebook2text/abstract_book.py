f8f3233f161b6009cb802a9430cd8f8c791ca13b
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-05-30 02:34:31 -0600

    REFACTOR: Add genertic types to abstract class

--- ebook2text/abstract_book.py
+++ ebook2text/abstract_book.py
@@ -1,44 +1,55 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import Generic, List, TypeVar
 
-from ._types import SplitType
 from .text_conversion import desmarten_text
 
+T = TypeVar("T")
 
-class BookConversion(ABC):
+
+class BookConversion(ABC, Generic[T]):
     def __init__(self, file_path: str, metadata: dict):
         self.file_path: str = file_path
         self.metadata: dict = metadata
         self.book = self._read_file(file_path)
 
     @abstractmethod
-    def _read_file(file_path: str):
+    def _read_file(self, file_path: str) -> T:
         raise NotImplementedError("Must be implemented in child class")
 
     @abstractmethod
     def split_chapters(self) -> str:
         raise NotImplementedError("Must be implemented in child class")
 
-
-class ImageExtraction(ABC):
     @abstractmethod
-    def extract_images(self) -> List[str]:
-        raise NotImplementedError("Must be implemented in child class")
-
-
-class TextExtraction(ABC):
-    @abstractmethod
-    def extract_text(self) -> str:
+    def extract_text(self, T) -> str:
         raise NotImplementedError("Must be implemented in child class")
 
     @abstractmethod
-    def _extract_image_text(self) -> str:
+    def extract_images(self, T) -> List[str]:
         raise NotImplementedError("Must be implemented in child class")
 
 
-class ChapterSplit(ABC):
+class ImageExtraction(ABC, Generic[T]):
     @abstractmethod
-    def __init__(self, text_obj: SplitType, metadata: dict, parent) -> None:
+    def extract_images(self, T) -> List[str]:
+        raise NotImplementedError("Must be implemented in child class")
+
+
+class TextExtraction(ABC, Generic[T]):
+    @abstractmethod
+    def extract_text(self, T) -> str:
+        raise NotImplementedError("Must be implemented in child class")
+
+    @abstractmethod
+    def _extract_image_text(self, T) -> str:
+        raise NotImplementedError("Must be implemented in child class")
+
+
+class ChapterSplit(ABC, Generic[T]):
+    @abstractmethod
+    def __init__(
+        self, text_obj: T, metadata: dict, parent: BookConversion
+    ) -> None:
         self.text_obj = text_obj
         self.metadata = metadata
         self.parent = parent
