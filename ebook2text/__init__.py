473896adea414618c8d640ed99ce74d11369d0ff
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2024-11-30 16:08:30 -0700

    remove no longer needed file handler functions

--- ebook2text/__init__.py
+++ ebook2text/__init__.py
@@ -1,13 +1,10 @@
 from ._logger import logger, set_logger
 from .convert_file import convert_file
-from .file_handling import read_text_file, write_to_file
 from .VERSION import __version__
 
 __version__ = __version__
 __all__ = [
     "convert_file",
-    "read_text_file",
-    "write_to_file",
     "logger",
     "set_logger",
 ]
--- ebook2text/file_handling.py
+++
@@ -1,9 +0,0 @@
-def read_text_file(file_path: str) -> str:
-    with open(file_path) as f:
-        read_file = f.read()
-    return read_file
-
-
-def write_to_file(content: str, file: str):
-    with open(file, "w", encoding="utf-8") as f:
-        f.write(content)
