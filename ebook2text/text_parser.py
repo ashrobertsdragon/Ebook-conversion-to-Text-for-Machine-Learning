9478dcfef3ca0ccac8c20bbb60101a71195021f7
Ashlynn Antrobus <ashrobertsantrobus@gmail.com>
2025-01-06 21:55:37 -0700

    Add exception

--- ebook2text/text_parser.py
+++ ebook2text/text_parser.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from typing import Generator
 
+from ebook2text._exceptions import TextConversionError
+from ebook2text._logger import logger
 from ebook2text.chapter_check import is_chapter
 from ebook2text.text_utilities import clean_text, desmarten_text
 
@@ -11,8 +13,12 @@ class TextParser:
         self.chapter_separator: str = "***"
 
     def read_line(self) -> Generator[str, None, None]:
-        with self.file_path.open("r", encoding="utf-8") as f:
-            yield from f
+        try:
+            with self.file_path.open("r", encoding="utf-8") as f:
+                yield from f
+        except (OSError, UnicodeDecodeError) as e:
+            logger.error(f"Error reading file: {e}")
+            raise TextConversionError from e
 
     def parse_file(self) -> Generator[str, None, None]:
         """
@@ -30,16 +36,50 @@ class TextParser:
 
             yield clean_text(parsed_line)
 
-    def write_text(self, content: str, file_path: Path) -> None:
+    def _clean_before_write(self, text: str, output_path: Path) -> str:
+        """
+        Strips the chapter separator from the text if the file does not exist.
+
+        Args:
+            text (str): The text to be cleaned.
+            output_path (Path): The path to the output file.
+
+        Returns:
+            str: The cleaned text.
+
+        Note:
+            This method is used to strip the leading chapter separator
+            before writing to a file.
+        """
+        return (
+            text
+            if output_path.exists()
+            else text.lstrip(self._chapter_separator)
+        )
+
+    def write_text(self, content: str, output_path: Path) -> None:
         """
         Write the parsed text to a file.
 
         Args:
-            text (str): The parsed text to be written to the file.
+            output (str): The parsed text to be written to the file.
+            file_path (Path): The path to the output file.
         """
-        with file_path.open("a", encoding="utf-8") as f:
-            f.write(content + "\n")
+        cleaned_content = self._clean_before_write(content, output_path)
+        with output_path.open("a", encoding="utf-8") as f:
+            f.write(cleaned_content + "\n")
 
     def return_string(self, generator: Generator[str, None, None]) -> str:
-        """Return the parsed text as a string."""
-        return "\n".join(line for line in generator if line.strip())
+        """
+        Return the parsed text as a string.
+
+        Args:
+            generator (Generator[str, None, None]): The content generator
+                that yields the text. This is usually the `parse_file` method.
+
+        Returns:
+            str: The parsed text as a single string.
+        """
+        return "\n".join(line for line in generator if line.strip()).lstrip(
+            self._chapter_separator
+        )
